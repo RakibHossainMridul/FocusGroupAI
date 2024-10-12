61ebda426089959a0000480cada86fdfc48f9b80
Md Rakib Hossain <86373736+RakibHossainMridul@users.noreply.github.com>
2024-10-12 21:02:29 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
-st.title('Focus Group AI')
+st.title('INSIGHT Chatbot')
+
+st.write('presented by Focus Group AI')
 
 st.info('This chat bot is collecting responses for our 2 new products: Green Tea and Shampoo')
