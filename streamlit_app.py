947ca850e56919339177c3183c743cf8e360f569
Md Rakib Hossain <86373736+RakibHossainMridul@users.noreply.github.com>
2024-10-12 23:58:04 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,40 @@
 import streamlit as st
+from langchain_core.messages import HumanMessage, AIMessage
+from dotenv import load_dotenv
 
-st.title('INSIGHT Chatbot')
+load_dotenv()
 
-st.write('presented by Focus Group AI')
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = []
 
-st.info('This chat bot is collecting responses for our 2 new products: Green Tea and Shampoo')
+st.set_page_config(page_title = "Insight Chatbot" )
+
+st.title("Insight Chatbot")
+
+
+
+
+
+#conversation
+for message in st.session_state.chat_history:
+    if isinstance(message,HumanMessage):
+        with st.chat_message("Human"):
+            st.markdown(message.content)
+    else:
+        with st.chat_message("AI"):
+            st.markdown(message.content)
+
+user_query = st.chat_input("Your message")
+
+if user_query is not None and user_query != "":
+    st.session_state.chat_history.append(HumanMessage(user_query))
+
+    with st.chat_message("Human"):
+        st.markdown("user_query")
+
+    with st.chat_message("AI"):
+        ai_response = ""
+        st.markdown(ai_response)
+
+    st.session_state.chat.history.append(AIMessage(ai_response))
+
